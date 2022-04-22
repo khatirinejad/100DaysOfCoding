7d669791f7788bb5857045294ae13736b058baa8
Mahyar Khatiri <31701811+khatirinejad@users.noreply.github.com>
2022-04-22 19:39:41 +0000

    testing

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 # 1. Create a greeting for your program.
-print("Welcome to the Band Name Generator.")
+
+#print("Welcome to the Band Name Generator.")
 # 2. Ask the user for the city that they grew up in.
-a = input("What's name of the city you grew up in?")
+#a = input("What's name of the city you grew up in?")
 # 3. Ask the user for the name of a pet.
-b = input("What's your pet's name?")
+#b = input("What's your pet's name? ")
 # 4. Combine the name of their city and pet and show them their band name.3666
-print("Your band name is " + a +" " +b)
+#print("Your band name is " + a +" " +b)
 
+#Data Types
 
+#String
+#street_name = "Abbey Road"
+#print(street_name[4] + street_name[7])
 
+#integer
+#print(2+3)
 
+#instead of comma for large number, we use underscore
+#print(124_245_3243)
+
+#Float
+#print(23.3)
+
+#Boolean (True,False)
+
+#age = (input("How old is your pet?"))
+#New_age = str(age)h
+#print("my pet is " + New_age + " years old." )
+
+#print(3 / 3 + 3 * 3 - 3)
+
+a = int("5") / int(2.7)
+print(type(a))
